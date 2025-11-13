33800d7dee9e7a1ce0e3c869ddc180fc2b947886
chaitanya <vamsi.blaze1@gmail.com>
2025-11-13 21:25:14 +0530

    fix: updated

--- roboshop-dev-infra/10-sg/main.tf
+++ roboshop-dev-infra/10-sg/main.tf
@@ -1,8 +1,21 @@
-module "vote_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+# this is using predefined modules
 
-  name        = "${local.common_name_prefix} - catalogue"
-  description = "Security group for catalogue with custom ports open within VPC, egress all traffic "
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value //tells AWS which VPC to create the security group in.
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
 
+#   name        = "${local.common_name_prefix} - catalogue"
+#   description = "Security group for catalogue with custom ports open within VPC, egress all traffic "
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value //tells AWS which VPC to create the security group in.
+
+# }
+
+
+module "sg" {
+  count          = length(var.sg_names)
+  source         = "git::https://github.com/ChaitanyaVamsi/module-terraform-aws-sg.git?ref=main" // refrence form github
+  project_name   = "roboshop"
+  environment    = "dev"
+  sg_name        = var.sg_names[count.index]
+  sg_description = "created for ${var.sg_names[count.index]}"
+  vpc_id         = local.vpc_id
 }
---
+++ roboshop-dev-infra/20- bastion/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.bastion_sg_id] # here we are linking by id , everytime a resource is created it also creates id, arn ,owner_id, tags_all
+  subnet_id              = local.public_subnet_id
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name} - ${var.environment} - bastion host"
+  })
+}
