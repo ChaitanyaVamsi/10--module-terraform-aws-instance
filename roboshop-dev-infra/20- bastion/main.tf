9c44435eeb8daaf5ac66087b32f31e16249062b1
chaitanya <vamsi.blaze1@gmail.com>
2025-12-04 00:17:21 +0530

    updateed catalogur

--- roboshop-dev-infra/20- bastion/main.tf
+++ roboshop-dev-infra/20- bastion/main.tf
@@ -3,15 +3,23 @@ resource "aws_instance" "example" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id] # here we are linking by id , everytime a resource is created it also creates id, arn ,owner_id, tags_all
   subnet_id              = local.public_subnet_id
-  iam_instance_profile   = aws_iam_instance_profile.bastion.name
-  user_data              = file("bastion.sh")
+  # iam_instance_profile   = aws_iam_instance_profile.bastion.name #first time
+  iam_instance_profile = "bastion_profile"
+
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+
+  user_data = file("bastion.sh")
   tags = merge(local.common_tags, {
     Name = "${var.project_name} - ${var.environment} - bastion host"
   })
 }
 
 
-resource "aws_iam_instance_profile" "bastion" {
-  name = "bastion_profile"
-  role = "bastionTerraformAdmin" // // The IAM role is created manually. We attach it to the Bastion instance profile.
-}
+# resource "aws_iam_instance_profile" "bastion" { # frist timme no error
+#   name = "bastion_profile"
+#   role = "bastionTerraformAdmin" // // The IAM role is created manually. We attach it to the Bastion instance profile.
+# }
--- roboshop-dev-infra/60- catalogue/main.tf
+++ roboshop-dev-infra/60- catalogue/main.tf
@@ -42,11 +42,114 @@ resource "aws_ec2_instance_state" "stop_my_instance" {
   depends_on  = [terraform_data.catalogue]
 }
 
-resource "aws_ami_from_instance" "create_ami" {
+resource "aws_ami_from_instance" "create_cataogue_ami" {
   name               = "${local.common_name_prefix}-catalogueAMI"
   source_instance_id = aws_instance.catalogue.id
   depends_on         = [aws_ec2_instance_state.stop_my_instance]
   tags = merge(local.common_tags, {
     Name = "${local.common_name_prefix}-catalogue" # roboshop-dev-catalogue
   })
+}
+
+# target group https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+resource "aws_lb_target_group" "catalogue_target_group" {
+  name                 = "${local.common_name_prefix}-catalogue" # roboshop-dev-catalogue
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 30 # waiting peridopo befreo deleting
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 20
+    matcher             = "200-299"
+    path                = "/health"
+    port                = "8080"
+    protocol            = "HTTP"
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
+
+}
+
+# launch template https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template
+resource "aws_launch_template" "catalogue_launch_template" {
+  name                                 = "${local.common_name_prefix}-catalogue"
+  image_id                             = aws_ami_from_instance.create_cataogue_ami.id
+  instance_initiated_shutdown_behavior = "terminate"
+
+  ram_disk_id = "test"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  # tags for instance
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(local.common_tags, {
+      Name = "${local.common_name_prefix}-catalogue" # roboshop-dev-catalogue
+    })
+  }
+
+  # tags for volume of instance
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(local.common_tags, {
+      Name = "${local.common_name_prefix}-catalogue" # roboshop-dev-catalogue
+    })
+  }
+
+  # tags for launch template
+  tags = merge(local.common_tags, {
+    Name = "${local.common_name_prefix}-catalogue" # roboshop-dev-catalogue
+  })
+
+}
+
+# auto scaling
+
+resource "aws_autoscaling_group" "catalogue_autoscaling" {
+  name                      = "${local.common_name_prefix}-catalogue"
+  max_size                  = 5
+  min_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue_launch_template.id
+    version = aws_launch_template.catalogue_launch_template.latest_version
+  }
+  vpc_zone_identifier = local.private_subnet_ids
+
+
+  dynamic "tag" {
+    for_each = merge(local.common_tags, {
+      Name = "${local.common_name_prefix}-catalogue" # roboshop-dev-catalogue
+    })
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue_autoscaling.name
+  name                   = "${local.common_name_prefix}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
 }
